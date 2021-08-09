e36b025658b2ce175e29373c0315b14c99e4e07d
Tom Pitt <tompitt94@gmail.com>
2021-08-09 23:57:39 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section">
+    <div class="container is-fluid has-text-centered">
+      <add-task @taskToAdd="addTask"/>
+    </div>
+    <hr>
+    <div>
+      <dl class="has-text-centered">
+        <dt v-for="task in tasks" :key="task.title" :style="task.complete ? 'text-decoration: line-through;' : ''">
+          {{ task.title }}
+      <!-- Complete -->
+          <span class="icon has-text-success ml-2 mt-2" v-if="!task.complete" @click="task.complete = true">
+            <i class="fas fa-check-circle is-clickable"></i>
+          </span>
+      <!-- Delete -->
+          <span class="icon has-text-danger ml-2 mt-2" @click="this.tasks.pop(task)">
+            <i class="fas fa-trash is-clickable"></i>
+          </span>
+        </dt>
+      </dl>
+    </div>
+  </section>
+</template>
+
+<script>
+import AddTask from './components/AddTask.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AddTask
+  },
+  data () {
+    return {
+      tasks: []
+    }
+  },
+  methods: {
+    addTask (eventData) {
+      const task = {
+        ...eventData.task
+      }
+      this.tasks.push(task)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="columns is-centered">
+    <div class="column is-two-fifths is-centered">
+      <form @submit.prevent="addTask">
+        <input class="input m-2 is-rounded has-text-centered" type="text" placeholder="Whatcha gonna do?" v-model="taskName">
+        <button class="button m-2 is-info is-rounded">Add Task</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data () {
+    return {
+      taskName: ''
+    }
+  },
+  methods: {
+    addTask () {
+      const task = {
+        title: this.taskName,
+        complete: false
+      }
+      this.$emit('taskToAdd', { task })
+
+      this.taskName = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
